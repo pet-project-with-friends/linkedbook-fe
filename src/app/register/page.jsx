6b452d4d9382b31be34a90397292c64c1de80ece
buitannguyen2612 <buitannguyen26122001@gmail.com>
2024-06-14 12:09:37 +0700

    add more validation for login form and register form

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,42 +1,49 @@
 "use client";
 import InputField from "@/src/components/inputField/page.jsx";
-import { Button } from "@material-tailwind/react";
 import Link from "next/link.js";
-import React from "react";
+import { useState } from "react";
 
 function Register() {
+  const [valueFeild, setValueFeild] = useState({
+    email: null,
+    userName: null,
+    password: null,
+    repassword: null,
+  });
+  const [cancel, setCancel] = useState(null);
+
+  const onRegister = () => {
+    if (cancel || Object.values(valueFeild).every((value) => value === null)) {
+      console.log("pls check all field");
+      return;
+    }
+    console.log("call api here");
+  };
+
   return (
     <div className="bg-backdrop w-full h-screen flex flex-col gap-5 justify-center items-center">
       <h1 className="text-[40px] max-w-fit">
         Make the most of your professional life
       </h1>
       <div className="w-[352px] p-[24px] bg-boxColor rounded-xl shadow-2xl flex flex-col items-center justify-center gap-5">
-        <form className="w-full h-auto ">
-          <InputField />
+        <form className="w-full h-auto flex flex-col gap-1">
+          <InputField
+            register
+            setValueFeild={setValueFeild}
+            valueFeild={valueFeild}
+            setCancel={setCancel}
+          />
         </form>
         <button
           className="w-full bg-buttonColor hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl focus:outline-none focus:shadow-outline"
           type="button"
+          onClick={() => onRegister()}
         >
           Sign Up
         </button>
         <p className="text-[10px] relative after:absolute after:h-[1px] after:w-32 after:bg-blue-300 after:left-5 after:top-1/2 before:absolute before:h-[1px] before:w-32 before:bg-blue-300 before:right-5 before:top-1/2">
           or
         </p>
-        <Button
-          size="sm"
-          variant="outlined"
-          color="blue-gray"
-          className="flex justify-center items-center gap-3 w-full rounded-xl"
-        >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="https://docs.material-tailwind.com/icons/google.svg"
-            alt="metamask"
-            className="h-6 w-6"
-          />
-          Continue with Google
-        </Button>
         <p className="text-[14px] ">
           Allready to linkein ?{" "}
           <Link
