d47269fc2028223c47dcb4a187fc98b1d8ca92c2
Nguyen <buitannguyen26122001@gmail.com>
2025-07-21 00:37:06 +0700

    [FE] [ARTICLE] Adding popup image

--- src/views/pages/home-page/components/ImageDialog.jsx
+++ src/views/pages/home-page/components/ImageDialog.jsx
@@ -1,16 +1,83 @@
+"use client";
 import { UseCondition } from "@/src/hooks/useCondition.jsx";
-import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import React, { useEffect } from "react";
+import Masonry from "react-masonry-css";
 
-const ImageDialog = ({ isOpen, isClose, blogId }) => {
+const ImageDialog = ({ isOpen, isClose, images = [] }) => {
+  const fakeImage = [
+    "https://picsum.photos/200/300?random=1",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=1",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=1",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=2",
+  ];
+  useEffect(() => {
+    const handleEsc = (e) => {
+      if (e.key === "Escape") isClose();
+    };
+    document.addEventListener("keydown", handleEsc);
+    return () => document.removeEventListener("keydown", handleEsc);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Define responsive breakpoints
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
   return (
     <UseCondition isTrue={isOpen}>
       <div
-        onClick={() => isClose()}
+        onClick={(e) => {
+          if (e.target.id === "container") isClose();
+        }}
         id="container"
-        className="fixed pt-5  inset-0 z-40 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center "
+        className="fixed inset-0 z-50 bg-black bg-opacity-80 backdrop-blur-sm flex justify-center items-center p-4"
       >
-        <div className="p-[20px] min-h-[22rem] max-h-[40rem] w-[727px] bg-boxColor rounded-xl flex flex-col justify-start gap-2">
-          hi this is my popup
+        <div className="relative w-full max-w-7xl max-h-[90vh] bg-white rounded-xl overflow-auto p-6">
+          {/* Close Button */}
+          <button
+            onClick={isClose}
+            className="absolute top-4 right-4 text-gray-600 hover:text-red-500 z-50"
+          >
+            <CloseIcon fontSize="large" />
+          </button>
+
+          <h2 className="text-xl font-semibold text-center mb-4">
+            Image Gallery
+          </h2>
+
+          {/* Masonry Layout */}
+          <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {fakeImage?.map((src, index) => (
+              <div key={index} className="mb-4">
+                {/* eslint-disable-next-line @next/next/no-img-element*/}
+                <img
+                  src={src}
+                  alt={`image-${index}`}
+                  className="w-full h-auto rounded-lg object-contain"
+                />
+              </div>
+            ))}
+          </Masonry>
         </div>
       </div>
     </UseCondition>
